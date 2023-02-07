bf809e7fccea468509e63da357e2ad3e7803016a
lbako801 <lbako98@gmail.com>
2023-02-06 23:44:17 -0700

    added animated icons, new UI design, and basic js

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,13 @@
+//API key used for requests
 const apiKey = '429fe2eb476f82ad84de7d98ea2ce8df';
 
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    // Accessing Geolocation of User
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+         
+      });
+    }
+  });
