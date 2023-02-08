a971d71ce5e87588178d0ff4b3c712abf1b96eef
lbako801 <lbako98@gmail.com>
2023-02-07 23:18:19 -0700

    added hover effects on forecast cards

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,13 @@
-document.getElementById("city-input").addEventListener("submit", function (event) {
-    event.preventDefault()
-});
+// const apiKey = '429fe2eb476f82ad84de7d98ea2ce8df';
 
-  
+function getCityName(event) {
+    event.preventDefault();
+    const searchValue = document.getElementById('city-input-value');
+    console.log(searchValue);
+    var cityName = searchValue.value;
+    console.log(cityName);
+}
 
+const cityForm = document.getElementById('city-input')
 
-
-
-
-
-  // const apiKey = '429fe2eb476f82ad84de7d98ea2ce8df';
+cityForm.addEventListener('submit', getCityName)
